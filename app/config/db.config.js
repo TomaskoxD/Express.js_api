74a202ae453b957103108506e1b399131f2d9269
TomaskoxD <tomo.ondrusek@gmail.com>
2024-02-09 13:40:07 +0100

    changed from localhost to hosting

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,13 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "express",
+//   PASSWORD: "express",
+//   DB: "expressAPI"
+// };
+
 module.exports = {
-  HOST: "localhost",
-  USER: "express",
-  PASSWORD: "express",
-  DB: "expressAPI"
+  HOST: "sql11.freemysqlhosting.net",
+  USER: "sql11683017",
+  PASSWORD: "RhBsw67lwl",
+  DB: "sql11683017"
 };
